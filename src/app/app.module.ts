17debe56af04a8fcfa1590a00dac97e22ceacab9
Cared24 <ponuszrichard93@gmail.com>
2019-10-14 15:56:07 +0200

    added header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FakeLogoComponent } from './fake-logo/fake-logo.component';
+import { CoursesComponent } from './courses-list/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FakeLogoComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule
