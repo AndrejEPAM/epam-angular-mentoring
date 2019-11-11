5e5c98da3ec320c463f5c127ac5bf64843fb0b41
Andrej Szasz <Andrej_Szasz@epam.com>
2019-11-11 18:19:56 +0100

    auto-fixed lint errors

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  footerText: string = "Copyright © Videocourses. All rights reserved."
+  footerText = 'Copyright © Videocourses. All rights reserved.';
 
   constructor() { }
 
