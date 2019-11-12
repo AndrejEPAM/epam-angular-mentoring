ea3af29d131ada0a24e07725e4bbefba1f380368
Andrej Szasz <Andrej_Szasz@epam.com>
2019-11-12 14:24:17 +0100

    added highlight directive that adds classes (basic tests)

--- src/app/courses-list/course-highlight.directive.ts
+++ src/app/courses-list/course-highlight.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, HostBinding } from '@angular/core';
+import { Course } from './course.model';
 
 @Directive({
-  selector: '[appCourseHighlight]'
+  selector: '[appCourseHighlight]',
 })
 export class CourseHighlightDirective {
+  @Input('appCourseHighlight') course!: Course;
 
   constructor() { }
+
+  @HostBinding('class.fresh') get isFresh() {
+    const currentDate = new Date();
+    const creationDate = new Date(this.course.creationDate);
+    const pastDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - 14);
+    return creationDate <= currentDate
+      && creationDate > pastDate;
+  }
+
+  @HostBinding('class.upcoming') get isUpcoming() {
+    const currentDate = new Date();
+    const creationDate = new Date(this.course.creationDate);
+    return creationDate > currentDate;
+  }
 
 }
