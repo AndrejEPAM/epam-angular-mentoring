2f23726cafe6a50d3fef22355fe8245f8988da97
Andrej Szasz <Andrej_Szasz@epam.com>
2019-11-12 14:49:35 +0100

    added no data display when no courses

--- src/app/courses-list/courses-list.component.spec.ts
+++ src/app/courses-list/courses-list.component.spec.ts
@@ -1,6 +1,7 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { Component, Input, Directive } from '@angular/core';
+import { Component, Input, Directive, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { CoursesListComponent } from './courses-list.component';
 import { CoursesService } from './courses.service';
@@ -59,7 +60,7 @@ describe('CoursesListComponent', () => {
     component = fixture.componentInstance;
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
 
@@ -71,4 +72,25 @@ describe('CoursesListComponent', () => {
     expect(getCoursesSpy).toHaveBeenCalled();
     expect(component.courses).toEqual(mockData);
   });
+
+  it('should display courses', () => {
+    const service = TestBed.get(CoursesService);
+    spyOn(service, 'getCourses').and.returnValue(mockData);
+
+    fixture.detectChanges();
+
+    const courseItems = fixture.debugElement.queryAll(By.directive(CourseItemStubComponent));
+    expect(courseItems.length).toBe(1);
+  });
+
+  it('should display message if no courses', () => {
+    fixture.detectChanges();
+
+    const divs = fixture.debugElement.queryAll(By.css('div'));
+    const text = divs.map((debugElement: DebugElement) => (debugElement.nativeElement as HTMLElement).textContent).join(' ');
+    console.log(text);
+    expect(text).toContain('No data');
+  });
+
+
 });
