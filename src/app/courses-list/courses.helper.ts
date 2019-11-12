e99ebf9328e7f6905a1cca7f6a2dd580f2e451a2
Andrej Szasz <Andrej_Szasz@epam.com>
2019-11-12 14:26:48 +0100

    added a working implementation without tests

--- src/app/courses-list/courses.helper.ts
+++ src/app/courses-list/courses.helper.ts
@@ -12,14 +12,14 @@ export let Courses: Course[] = [
     {
         id: 1,
         title: 'Video Course #02',
-        creationDate: '02, October, 2019',
+        creationDate: '12, November, 2019',
         duration: '45 min',
         description: 'Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.'
     },
     {
         id: 2,
         title: 'Video Course #02',
-        creationDate: '03, October, 2019',
+        creationDate: '1, November, 2100',
         duration: '2h 30 min',
         description: 'Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt.'
     }
