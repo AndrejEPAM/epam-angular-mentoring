b4db886d34457d6f86ed23951b3c16cbb2f64fb9
Andrej Szasz <v-aszasz@hotels.com>
2020-01-25 18:31:02 +0100

    Task 7.1, wiring add/edit component through routing

--- src/app/add-course/add-course.component.ts
+++ src/app/add-course/add-course.component.ts
@@ -1,22 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { takeUntil, switchMap } from 'rxjs/operators';
+import { Subject, of } from 'rxjs';
+import { CoursesService } from '../courses-list/courses.service';
+import { Course } from '../courses-list/course.model';
 
 @Component({
   selector: 'app-add-course',
   templateUrl: './add-course.component.html',
   styleUrls: ['./add-course.component.scss']
 })
-export class AddCourseComponent implements OnInit {
+export class AddCourseComponent implements OnInit, OnDestroy {
 
+  title = new FormControl();
+  description = new FormControl();
+  date = new FormControl();
   form = new FormGroup({
-    title: new FormControl(),
-    description: new FormControl(),
-    date: new FormControl(),
+    title: this.title,
+    description: this.description,
+    date: this.date,
   });
 
-  constructor() { }
+  private componentDestroyed = new Subject();
+  componentDestroyed$ = this.componentDestroyed.asObservable();
+
+  constructor(private activatedRoute: ActivatedRoute,
+    private coursesService: CoursesService) { }
 
   ngOnInit() {
+    this.activatedRoute.params.pipe(
+      switchMap((params) => of(this.coursesService.getCourse(params.id))),
+      takeUntil(this.componentDestroyed$)
+    ).subscribe((course) => {
+      if(course) {
+        this.loadValues(course);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.componentDestroyed.next();
   }
 
   onSubmit() {
@@ -25,5 +49,11 @@ export class AddCourseComponent implements OnInit {
 
   onCancel() {
     console.log('cancel');
+  }
+
+  loadValues(course: Course) {
+    this.title.patchValue(course.title);
+    this.description.patchValue(course.description);
+    this.date.patchValue(course.creationDate);
   }
 }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { CoursesListComponent } from './courses-list/courses-list.component';
+import { AddCourseComponent } from './add-course/add-course.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/courses', pathMatch: 'full' },
   { path: 'courses', component: CoursesListComponent },
+  { path: 'courses/new', component: AddCourseComponent },
+  { path: 'courses/:id', component: AddCourseComponent },
   { path: 'login', component: LoginComponent }
 ];
 
