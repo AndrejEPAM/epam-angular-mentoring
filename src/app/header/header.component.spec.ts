561c0d07e249cc60334aaa57d0bd3966cc7eb71c
Andrej Szasz <v-aszasz@hotels.com>
2020-01-27 02:37:09 +0100

    Task 7 Details 1: erase token + redirect to login page

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,6 +1,17 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header.component';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  template: `Login`
+})
+export class LoginComponent {
+}
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +19,13 @@ describe('HeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      imports: [
+        RouterTestingModule.withRoutes([
+          {path: '', redirectTo: 'courses', pathMatch: 'full'},
+          {path: 'login', component: LoginComponent},
+        ]),
+      ],
+      declarations: [ HeaderComponent, LoginComponent ]
     })
     .compileComponents();
   }));
@@ -19,7 +36,24 @@ describe('HeaderComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('logout logs out', () => {
+    const logoutMethod = spyOn(TestBed.get(AuthService), 'logout');
+    component.logout();
+    expect(logoutMethod).toHaveBeenCalled();
+  });
+
+  it('logout redirects to login page', fakeAsync(() => {
+    const router = TestBed.get<Router>(Router);
+    const location = TestBed.get<Location>(Location);
+    router.initialNavigation();
+
+    // implementation specific
+    component.logout();
+    tick();
+    expect(location.path()).toEqual('/login');
+  }));
 });
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from '../user.model';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
-  ngOnInit() {
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
-
 }
